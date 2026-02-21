26e7a50471690fe79bcec8ec266d61d3eb0cbbdd
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-02-21 10:43:56 +0700

    Refactor: redesign tampilan text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'w-full border-slate-200 rounded-2xl focus:border-emerald-500 focus:ring focus:ring-emerald-500/10 text-sm font-semibold text-slate-700 px-4 py-3 transition-all']) !!}>
